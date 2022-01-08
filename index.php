5278a3007132664bda1ef46f55f781913426f55a
Noah Jauchmann <noah.jauchmann@stud.th-deg.de>
2022-01-08 20:23:19 +0100

    better vote system, changed style

--- index.php
+++ index.php
@@ -9,15 +9,13 @@ session_start(); // gets session id
 if (session_id() == '' || !isset($_SESSION['signed_in'])) { // if not logged in 
     echo'<br>You are currently not logged in login <a href="./login.php">here</a>.';
 } else { // if logged in 
-    echo "<div class=window>";
     echo "<h4>Account:</h4>";
     echo "username: " . $_SESSION['username'] . "<br>";
     echo "user_id: " . $_SESSION['user_id'] . "<br>";
     echo "logged_in: " . $_SESSION['signed_in'] . "<br>";
     echo "admin: " . $_SESSION['is_admin'] . "<br>";
     echo "<hr>";
-    echo "</div>";
-    
+
     // action: create post
     if(isset($_POST['createpost']))
     {
@@ -32,51 +30,73 @@ if (session_id() == '' || !isset($_SESSION['signed_in'])) { // if not logged in
         }
     }
 
+        // HTML - form for writing and sending post
+        echo'
+        <div class="createpost">
+        <form action="" method="post" id="submitpost">
+            <input type="text" id="title" name="title" value="" placeholder="Title" required><br>
+            <input type="text" id="text" name="text" value="" placeholder="Text" required><br>
+            <input type="submit" id="createpost" class="createpost" name="createpost" value="Create Post">
+        </form>
+        </div>';
+        echo "<hr>";
+
     // Selects all posts
-    // TODO: add upvotes / downvotes together
-    $sql = "SELECT post.creation_date, user.username, post.title, post.content FROM post LEFT JOIN user ON post.author = user.user_id ORDER BY creation_date DESC;";
-    // Displays all posts and puts it into a window
+    $sql = "SELECT post.creation_date, post.post_id, user.username, post.title, post.content FROM post LEFT JOIN user ON post.author = user.user_id ORDER BY creation_date DESC;";
+    // Displays all posts
     if ($res = mysqli_query($conn, $sql)) {
         echo "<div class='window scroll forum'>";
         // Goes trough all rows and creates the post bubbles
         while ($row = mysqli_fetch_array( $res, MYSQLI_ASSOC))
         {   
             $creation_date = $row['creation_date'];
+            $post_id = $row['post_id'];
             $username = $row['username'];
             $title= $row['title'];
             $content = $row['content'];
-            
+
             echo "<div class='post'>";
 
-            // TODO: delete post if admin
-            // DELETE FROM post WHERE post.post_id = $post_id;
-            if ($_SESSION['is_admin']){
-                echo '<a href="delete"><i class="material-icons">delete</i></a>';
+            echo "<div class='left'>";
+            // Vote
+            // TODO: implement upvote downvote
+            // SELECT SUM(`vote`) FROM `user_post` WHERE `post_id` = '1'
+            $sql_vote = "SELECT SUM(vote) AS 'votes' FROM user_post WHERE post_id = $post_id;";
+            $result = mysqli_query($conn, $sql_vote);
+            $row = mysqli_fetch_array( $result, MYSQLI_ASSOC);
+            
+            echo '<a href="expand_less"><i class="material-icons">expand_less</i></a><br>';
+            if ($row['votes'] <> NULL) {
+                echo "$row[votes]";
+            }else{
+                echo "0";
             }
-                echo "<h4>$title</h4>";
-                echo "<div>$content</div>";
-                echo "<div class='left'>By $username</div>";
-                echo "<div class='right'>$creation_date</div>";
+            echo '<br><a href="expand_less"><i class="material-icons">expand_more</i></a><br>';
+
+            // DELETE FROM post WHERE post.post_id = $post_id;
+            // TODO: delete post if admin
+            if ($_SESSION['is_admin']){
+                echo '<br><a href="delete"><i class="material-icons">delete</i></a><br>';
+            }
+            echo "</div>"; // end of left
+
+            echo "<div class='right'>";
+            echo "<h4>$title</h4>";
+            echo "$content<br>";
+            echo "By $username<br>";
+            echo "$creation_date<br>";
+            echo "</div>"; // end of right
+            echo' <br style="clear:both;"/>'; // fixes problem where post is 0px
+
             echo "</div>"; // end of post
-            echo "<br>";
+
         }
         echo "</div>"; // end of forum scroll
         mysqli_free_result($res);
 
     }else {
-        echo "ERROR: Could not able to execute<br>";
+        echo "ERROR: Could't execute<br>";
     }
-    
-
-    // HTML - form for writing and sending post
-    echo'
-    <div class="window">
-    <form action="" method="post" id="submitpost">
-        <input type="text" id="title" name="title" value="" placeholder="Title" required><br>
-        <input type="text" id="text" name="text" value="" placeholder="Text" required><br>
-        <input type="submit" id="createpost" class="createpost" name="createpost" value="Create Post">
-    </form>
-    </div>';
 
 }
 
