579eb08167ca0b234bda5acc63d33d20caab16df
Noah Jauchmann <noah.jauchmann@stud.th-deg.de>
2022-01-08 13:35:51 +0100

    added favicon, added delete button, cleaned up css

--- header.php
+++ header.php
@@ -7,6 +7,11 @@
     <meta name="description" content="This is a cool forum." />
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="style.css" type="text/css">
+    <link rel="apple-touch-icon" sizes="180x180" href="src/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="src/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="src/favicon-16x16.png">
+    <link rel="manifest" href="src/site.webmanifest">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 </head>
 
 <body>
