8ec285b372dab847f39a56aafd6c7aec0dd2e792
Noah Jauchmann <noah.jauchmann@stud.th-deg.de>
2022-01-09 15:05:11 +0100

    improved account page, added profile picture

--- account.php
+++ account.php
@@ -9,12 +9,17 @@ session_start(); // gets session id
 if (session_id() == '' || !isset($_SESSION['signed_in'])) { // if not logged in 
     echo'<br>You are currently not logged in login <a href="./login.php">here</a>.';
 } else { // if logged in 
-    echo "<h4>Account:</h4>";
+    echo "<div class='account'>";
+    echo "<h1>Account:</h1>";
+    echo "<img src='upload/default.png'><br>";
     echo "username: " . $_SESSION['username'] . "<br>";
     echo "user_id: " . $_SESSION['user_id'] . "<br>";
     echo "logged_in: " . $_SESSION['signed_in'] . "<br>";
     echo "admin: " . $_SESSION['is_admin'] . "<br>";
-    echo "<hr>";
+    echo "</div>";
+
+    // TODO: fileupload for profile picture
+
 }
 
 include 'footer.php';
