666940c4104f90534bf9213e9036df71ba780d38
jk05234 <jasmin.kufner@stud.th-deg.de>
2022-01-12 12:11:27 +0100

    Trying to upload without selecting file won't throw error anymore, added css to the messages of file upload

--- account.php
+++ account.php
@@ -62,45 +62,57 @@ if (session_id() == '' || !isset($_SESSION['signed_in'])) { // if not logged in
         $uploadOk = 1;
         $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
 
-        // Checks if image is really an image
-        $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
-        if($check !== false) {
-            echo "File is an image - " . $check["mime"] . ".<br>";
-            $uploadOk = 1;
-        } else {
-            echo "File is not an image.<br>";
-            $uploadOk = 0;
-        }
-
-        // Checks if file already exists
-        if (file_exists($target_file)) {
-            echo "Sorry, file already exists.<br>";
-            $uploadOk = 0;
-        }
-
-        // Checks if file size > 500KB
-        if ($_FILES["fileToUpload"]["size"] > 500000) {
-            echo "Sorry, your file is too large.<br>";
-            $uploadOk = 0;
-        }
-
-        // Allows only certain file formats
-        if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg" && $imageFileType != "gif" ) {
-            echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.<br>";
-            $uploadOk = 0;
-        }
-
-        // Check if $uploadOk is set to 0 by an error
-        // Else, if everything is ok, try to upload file or send error message if something went wrong
-        if ($uploadOk == 0) {
-            echo "Sorry, your file was not uploaded.<br>";
-        } else {
-            if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
-                echo "The file ". htmlspecialchars( basename( $_FILES["fileToUpload"]["name"])). " has been uploaded.<br>"; 
+        // Checks if file path has been selected
+        if(empty($target_file) OR empty($imageFileType)){
+            echo '<div class="message">';
+            echo "Please select a file first<br>";
+            echo '</div>';
+        } else { //if file selected
+            echo '<div class="message">';
+    
+            // Checks if image is really an image
+            $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
+            if($check !== false) {
+                echo "File is an image - " . $check["mime"] . ".<br>";
+                $uploadOk = 1;
             } else {
-                echo "Sorry, there was an error uploading your file.<br>";
+                echo "File is not an image.<br>";
+                $uploadOk = 0;
             }
+
+            // Checks if file already exists
+            if (file_exists($target_file)) {
+                echo "Sorry, file already exists.<br>";
+                $uploadOk = 0;
+            }
+
+            // Checks if file size > 500KB
+            if ($_FILES["fileToUpload"]["size"] > 500000) {
+                echo "Sorry, your file is too large.<br>";
+                $uploadOk = 0;
+            }
+
+            // Allows only certain file formats
+            if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg" && $imageFileType != "gif" ) {
+                echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.<br>";
+                $uploadOk = 0;
+            }
+
+            // Check if $uploadOk is set to 0 by an error
+            // Else, if everything is ok, try to upload file or send error message if something went wrong
+            if ($uploadOk == 0) {
+                echo "Sorry, your file was not uploaded.<br>";
+            } else {
+                if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+                    echo "The file ". htmlspecialchars( basename( $_FILES["fileToUpload"]["name"])). " has been uploaded.<br>"; 
+                } else {
+                    echo "Sorry, there was an error uploading your file.<br>";
+                }
+            }
+            echo '</div>';
+
         }
+
     }
 
 }
